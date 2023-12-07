767606a890f93af7fc9a0c3cee9f61f9f21a4482
efe33 <efe33@BPC04>
2023-12-07 13:17:15 +0300

    Proje dosyası ekle.

---
+++ SıraSızde/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SıraSızde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ekranIslem ei  = new ekranIslem();
+            ei.EkranaYaz(0);
+            
+            Console.WriteLine("========================");
+            ei.EkranaYaz();
+        }
+    }
+    class ekranIslem
+    {
+        public void EkranaYaz(params int[] sayilar)
+        {
+            if (sayilar.Length == 0)
+            {
+                Console.WriteLine("parametre olmadığı için metotdan çıkıyor");
+                return;
+                
+            }
+            Console.WriteLine("parametreden gelen değerler");
+            foreach(var s in sayilar)
+            {
+                if (s==0)
+                {
+                    return;
+
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+    
+}
